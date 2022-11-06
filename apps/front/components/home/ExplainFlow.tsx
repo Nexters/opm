2952740f57aad7b1a6d4d3498943acd34aeaba54
anyl92 <yulim9592@gmail.com>
2022-11-06 15:55:33 +0900

    add slidebox in home

--- apps/front/components/home/ExplainFlow.tsx
+++ apps/front/components/home/ExplainFlow.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
@@ -10,6 +10,41 @@ const ExplainFlow: FunctionComponent = () => {
   const handlePosts = () => {
     router.push("/posts");
   };
+
+  const slideContainerRef = useRef<HTMLDivElement>(null);
+
+  const slideList = [
+    {
+      video: "/home1.mp4",
+      description:
+        "Please select a category and register your writing. We'll match the editor to you.",
+    },
+    {
+      video: "/home2.mp4",
+      description:
+        "Chat allows you to communicate with the editor while the text is being modified.",
+    },
+  ];
+  const slideCount = slideList.length;
+
+  const [slideWidth, setSlideWidth] = useState<number>(0);
+  const [slideLeft, setSlideLeft] = useState<number>(0);
+  useEffect(() => {
+    if (slideContainerRef.current) {
+      setSlideWidth(slideContainerRef.current.clientWidth * -1);
+    }
+  }, []);
+
+  const handlePrev = () => {
+    if (slideLeft === 0) return;
+    setSlideLeft(slideLeft - slideWidth);
+  };
+  const maxSlideLeft = slideWidth * (slideCount - 1);
+  const handleNext = () => {
+    if (slideLeft === maxSlideLeft) return setSlideLeft(0);
+    setSlideLeft(slideLeft + slideWidth);
+  };
+
   return (
     <div className={styles.explainContainer}>
       <div className={styles.editingList}>
@@ -17,34 +52,43 @@ const ExplainFlow: FunctionComponent = () => {
           <h1 className={styles.explainText}>
             Affordable prices, Quality services.
           </h1>
-          <h1 className={styles.explainText}>
+          <h2 className={styles.explainText}>
             Native editors from English-speaking countries to help you get done
             with your work.
-          </h1>
+          </h2>
         </div>
         <div className={styles.imageContainer}>
-          <div>
-            <Image src="/backbutton.png" alt="logo" width={40} height={40} />
+          <div className={styles.slideButton} onClick={handlePrev}>
+            <Image src="/backbutton.png" alt="logo" width={200} height={200} />
           </div>
-          <div className={styles.imageBox}>
-            <video
-              src="/home1.mp4"
-              className={styles.videoSection}
-              autoPlay
-              muted
-              loop
-              width="90%"
-              height="70%"
-            />
+          <div className={styles.slideContainer} ref={slideContainerRef}>
+            {slideList.map((slide) => (
+              <div
+                key={slide.video}
+                className={styles.slideBox}
+                style={{ left: slideLeft }}
+              >
+                <div className={styles.imageBox}>
+                  <video
+                    src={slide.video}
+                    className={styles.videoSection}
+                    autoPlay
+                    muted
+                    loop
+                    width="90%"
+                    height="70%"
+                  />
+                </div>
+                <div
+                  style={{ width: "40%", margin: "auto", fontWeight: "200" }}
+                >
+                  <div>{slide.description}</div>
+                </div>
+              </div>
+            ))}
           </div>
-          <div style={{ width: "40%", fontWeight: "200" }}>
-            <div>
-              Please select a category and register your writing. <br />
-              We&apos;ll match the editor to you.
-            </div>
-          </div>
-          <div style={{ transform: "rotate(180deg)" }}>
-            <Image src="/backbutton.png" alt="logo" width={40} height={40} />
+          <div style={{ transform: "rotate(180deg)" }} onClick={handleNext}>
+            <Image src="/backbutton.png" alt="logo" width={50} height={50} />
           </div>
         </div>
         <div>
